1674ded4d13d87dcb39680e15e822f56f063ef3a
Protaga <101981115+Protaga@users.noreply.github.com>
2023-11-20 16:13:44 +0200

    Update 4-Program-Methods-Example.sql

--- 3-SoftwareConstruction/4-Program-Methods-Example.sql
+++ 3-SoftwareConstruction/4-Program-Methods-Example.sql
@@ -3,14 +3,10 @@ CREATE OR REPLACE FUNCTION order_purifier (
     p_cost IN INT
 )
 RETURN BOOLEAN IS
-    v_name purifier.name%Type := upper(SUBSTR(p_name, 1, 100));
+    v_name purifier.name%Type := upper(p_name);
     v_cost purifier.cost%Type := p_cost;
-    v_purifier_id purifier.purifier_id%Type; 
+    v_purifier_id purifier.purifier_id%Type;
 BEGIN
-    IF LENGTH(v_name) > 100 OR v_cost <= 0 THEN
-        RETURN FALSE;
-    END IF;
-
     BEGIN
         SELECT purifier_id INTO v_purifier_id FROM purifier WHERE name = v_name AND cost = v_cost;
         RETURN TRUE;
