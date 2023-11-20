8c3184f189e66bb13d6b13361ff222504f8cb06d
Protaga <101981115+Protaga@users.noreply.github.com>
2023-11-20 14:08:58 +0200

    Update 3-Program-Сlasses-Example.sql

--- 3-SoftwareConstruction/3-Program-Сlasses-Example.sql
+++ 3-SoftwareConstruction/3-Program-Сlasses-Example.sql
@@ -1,1 +1,33 @@
+CREATE OR REPLACE TYPE User_ AS OBJECT (
+    num_card INT,        
+    valid_per VARCHAR(10),       
+    cvv2 INT,
+    NOT FINAL MEMBER PROCEDURE display
+) NOT FINAL 
+/
+
+CREATE TYPE UserInfo IS TABLE OF User_;
+/
+
+CREATE OR REPLACE TYPE Client UNDER User_ (
+    telephone INT, 
+    info UserInfo,       
+    OVERRIDING MEMBER PROCEDURE display
+); 
+/
+
+CREATE OR REPLACE TYPE Operator AS OBJECT (
+    notify Client,
+    MEMBER PROCEDURE display
+); 
+/
+
+CREATE OR REPLACE TYPE Purifier AS OBJECT (
+    name VARCHAR(100),
+    cost INT,
+    order Client,
+    MEMBER PROCEDURE display
+); 
+/
+
 
